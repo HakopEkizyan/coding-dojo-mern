75b013859d1d81feb81f5355c764c5fc1aa3830b
Hakop Ekizyan <65827454+HakopEkizyan@users.noreply.github.com>
2022-11-04 22:15:18 -0700

    Add files via upload

---
+++ react/intro/first-react-jsx/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="header-tag">Hello Dojo!</h1>
+        <div>
+          <h3>Things I need to do:</h3>
+          <ul>
+            <li>Learn React</li>
+            <li>Climb Mt.Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
