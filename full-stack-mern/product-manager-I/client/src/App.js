a2ef62b489c7d96fb26138b9ee322bdf7a505d6d
Hakop Ekizyan <65827454+HakopEkizyan@users.noreply.github.com>
2022-12-15 21:12:49 -0800

    Add files via upload

---
+++ full-stack-mern/product-manager-I/client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ProductForm from './components/ProductForm';
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <div>
+      <ProductForm/>
+    </div>
+  );
+}
+
+export default App;
